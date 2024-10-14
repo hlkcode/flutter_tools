21bb30235be7a050d106f0b572308713fa94236c
Halik <hlkcode@gmail.com>
2024-10-14 17:03:39 +0000

    removed unused properties

--- lib/common.dart
+++ lib/common.dart
@@ -12,11 +12,9 @@ const Color kErrorColor = Color(0xCCDC3232);
 const Color kSuccessColor = Color(0xFF78E000);
 
 dynamic get user => GetStorage().read(Constants.USER);
-int get timeZoneOffset => DateTime.now().toLocal().timeZoneOffset.inHours;
-
-const numberPerPage = 10;
 
 dynamic get userData => GetStorage().read(Constants.USER_DATA);
+
 Map<String, String> get headers {
   final uData = GetStorage().read(Constants.USER_DATA);
   final token = uData[Constants.TOKEN_KEY];
